f62dc56c818613a745bcbc3402cc39a5f83f55d8
Furkan Cebeci <funcexi@gmail.com>
2022-06-25 18:04:46 +0300

    first commit

---
+++ src/main/java/com/furkancebeci/interprobefirsthomework/InterprobeFirstHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.furkancebeci.interprobefirsthomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterprobeFirstHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterprobeFirstHomeworkApplication.class, args);
+	}
+
+}
